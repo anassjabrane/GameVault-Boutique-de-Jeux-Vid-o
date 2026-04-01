cdd850357beafd30dfedd8dd584118bdb2e4106f
Anass Jabrane <jabraneanass12@gmail.com>
2026-04-01 12:05:49 +0100

    fixer la logique d'affichages des jeux

--- app.js
+++ app.js
@@ -2,27 +2,35 @@
 import { games } from "./data.js";
 
 const grid = document.getElementById('game-grid');
-const search = document.getElementById('search-input');
+const search = document.getElementById('search_input');
 
 // Debugging
-console.log('Games imported:', games);
-console.log('Grid element:', grid);
+// console.log('Games imported:', games);
+// console.log('Grid element:', grid);
 
 // afichage::::::::::
-function showGames() {
-    if (!grid) {
-        console.error('Game grid element not found');
-        return;
-    }
-    grid.innerHTML = games.map(game => `
-        <div class="bg-gray-900 p-4 rounded-xl border border-gray-800">
-            <img src="${game.image}" class="w-full h-40 object-cover rounded">
-            <h3 class="mt-2 font-bold">${game.title}</h3>
-            <p class="text-green-500">${game.price} $</p>
-            <button onclick="addToCart(${game.id})" class="bg-blue-600 w-full mt-2 py-1 rounded">Add</button>
-        </div>
-    `).join('');
+function showGames(list) {
+    grid.innerHTML = ""; 
+    
+    list.forEach(game => {
+        grid.innerHTML += `
+            <div class="bg-gray-900 p-4 rounded-xl border border-gray-800">
+                <img src="${game.image}" class="w-full h-40 object-cover rounded">
+                <h3 class="mt-2 font-bold">${game.title}</h3>
+                <p class="text-green-500">${game.price} $</p>
+                <button class="bg-blue-600 w-full mt-2 py-1 rounded">Add</button>
+            </div>
+        `;
+    });
 }
+//  Recherche Dynamique
+
+
+
+
+
 
 // Call the function to render games
-showGames();
+showGames(games);
+
+
